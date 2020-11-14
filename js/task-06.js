bc283e517c0dc6aa7ff4e1fdd1db06e3ff86d044
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2020-11-15 00:45:18 +0200

    исправляет замечания в task-06

--- js/task-06.js
+++ js/task-06.js
@@ -5,9 +5,10 @@ inputReef.addEventListener(`blur`, chsngeColorInput);
 function chsngeColorInput(e) {
   const dataLength = Number(inputReef.getAttribute(`data-length`));
   const currentLengthInput = e.currentTarget.value.length;
-  dataLength === currentLengthInput
-    ? inputReef.classList.add(`valid`) || inputReef.classList.remove(`invalid`)
-    : inputReef.classList.add(`invalid`) || inputReef.classList.remove(`valid`);
-
-  currentLengthInput === 0 ? inputReef.classList.remove(`invalid`) : undefined;
+  if (dataLength === currentLengthInput) {
+    inputReef.classList.add(`valid`) || inputReef.classList.remove(`invalid`);
+  } else {
+    inputReef.classList.add(`invalid`) || inputReef.classList.remove(`valid`);
+  }
+  if (currentLengthInput === 0) inputReef.classList.remove(`invalid`);
 }
